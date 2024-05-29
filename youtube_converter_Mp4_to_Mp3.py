e2e7338edb9f7f9344b49ba18a541013f164b4ca
RAMIHAJARIMANANA Herijaona Mampionona <108388110+HAJA-M@users.noreply.github.com>
2024-05-29 23:25:01 +0300

    Ajout youtube_converter_Mp4_to_Mp3.py et barre de progression
    Ajout de la fichier youtube_converter_Mp4_to_Mp3.py et ajout de la barre de progression de téléchargement

--- youtube_converter_Mp4_to_Mp3.py
+++ youtube_converter_Mp4_to_Mp3.py
@@ -6,8 +6,7 @@ L'application permet aux utilisateurs de télécharger facilement des vidéos Yo
 L'interface intuitive et esthétique, grâce à CustomTkinter, offre une expérience utilisateur agréable et efficace.
 
 Modification à apporter :
-gestion des erreurs lors du téléchargement de la vidéo [1]
-Ajout d'une barre de progression pour indiquer l'avancement du téléchargement de la vidéo [1]
+gestion des erreurs lors du téléchargement de la vidéo
 
 '''
 
@@ -21,37 +20,23 @@ customtkinter.set_default_color_theme("blue")
 #fenêtre
 root = customtkinter.CTk()
 root.title("YouTube Video Downloader V1.0")
-root.geometry("500x300")
+root.geometry("400x210")
 
 #function
 def telecharger():
     try: 
         YtLink = lien.get()
-        Ytobjet = YouTube(YtLink, on_progress_callback=encours)
-        video = Ytobjet.streams.get_highest_resolution()
+        Ytobjet = YouTube(YtLink)
+        video = Ytobjet.streams.get_audio_only()
         titre.configure(text=Ytobjet.title + " est en cours de téléchargement ...")
         video.download()
-        Tterminer.configure(text="Video téléchargée avec succès !", text_color="green")
-        print("Téléchargement OK !")
 
     except:
-        Tterminer.configure(text="Une erreur est survenue lors du téléchargement de la vidéo.", text_color="red")
+        Tterminer.configure("Une erreur est survenue lors du téléchargement de la vidéo.", text_color="red")
         print("Une erreur est survenue lors du téléchargement de la vidéo.")
 
-def encours(stream, chunk, bytes_remaining):
-    taille_total = stream.filesize
-    byte_telecharger = taille_total - bytes_remaining
-    pourcentage = byte_telecharger / taille_total * 100
-    print(f"{pourcentage}%")
-    valeur_pourcentage = str(int(pourcentage))
-    textPourcentage.configure(text=valeur_pourcentage + "%")
-    textPourcentage.update()
-
-    #barre de progression mise à jour
-    barre.set(float(pourcentage)/100)
-
-    if pourcentage == 100:
-        barre.configure(progress_color="green")
+    Tterminer.configure(text="Video téléchargée avec succès !", text_color="green")
+    print("Téléchargement OK !")
 
 #titre
 titre = customtkinter.CTkLabel(root, text="Votre lien YouTube :")
@@ -65,14 +50,6 @@ lien.pack(padx=10, pady=10)
 #telechargement terminer
 Tterminer = customtkinter.CTkLabel(root, text="")
 Tterminer.pack(padx=10, pady=10)
-
-#barre de progression
-barre = customtkinter.CTkProgressBar(root, width=500, height=5,progress_color="red")
-barre.set(0)
-barre.pack(padx=10, pady=10)
-
-textPourcentage = customtkinter.CTkLabel(root, text="0%")
-textPourcentage.pack(padx=10, pady=10)
 
 #bouton
 btn1 = customtkinter.CTkButton(root, text="Télécharger", command=telecharger)
